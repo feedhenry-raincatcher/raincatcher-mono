dd00600c359a7a9c2055d09b1a46941b1c10a3d9
spriadka <spriadka@github.com>
2017-03-15 11:15:28 +0100

    added tests for controller

---
+++ test/mapController-spec.js
@@ -0,0 +1,49 @@
+var angular = require('angular');
+require('angular-mocks');
+
+var Promise = require('promise');
+var sinon = require('sinon');
+
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
+const CONSTANTS = require('../lib/angular/constants');
+var initMapDirectiveModule = require('./initMapDirectiveModule');
+
+describe('Map Controller',function(){
+
+    var sampleData = [{
+        id: 'Lot of Work',
+        description: 'Should be done'
+    },{
+        id: 'Some',
+        description: 'Should not'
+    }];
+
+    before(function(){
+        initMapDirectiveModule();
+        require('../lib/angular/controller')
+    });
+    beforeEach(angular.mock.module(CONSTANTS.MAP_DIRECTIVE));
+
+    beforeEach(inject(function(_$controller_){
+        this.$controller = _$controller_;
+    }));
+
+    it('Should set controller values',inject(function($rootScope){
+        var mediatorSpy = sinon.spy();
+        var $scope = $rootScope.$new();
+        var promise = Promise.resolve(sampleData);
+        var listWorkordersStub = sinon.stub().returns(promise);
+        var mapMediatorServiceMock = {
+            listWorkorders: listWorkordersStub
+        };
+        var mapController = this.$controller('MapController',{$scope : $scope, mediator: mediatorSpy, mapMediatorService: mapMediatorServiceMock});
+        expect($scope.workorders).to.eventually.equal(sampleData);
+        sinon.assert.calledOnce(listWorkordersStub);
+    }));
+
+
+});
